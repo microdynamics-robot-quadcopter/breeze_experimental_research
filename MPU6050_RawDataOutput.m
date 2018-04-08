104566ad22c2680e3c65bd6eaa6b3db978533488
maksyuki <myc20404401@sina.com>
2018-04-08 16:59:45 +0800

    Modify MPU6050 Raw Data Output file

--- MPU6050_RawDataOutput.m
+++ MPU6050_RawDataOutput.m
@@ -1,29 +1,54 @@
 clear all;
 clc;
 
-data2=load('./MPU6050_RawGyroData.txt'); 
-x=data2(:,1);
-y1=data2(:,2);
-y2=data2(:,3);
-y3=data2(:,4);
+is_gyro = 1;
 
-subplot(3, 1, 1);
-plot(x,y1);
-xlabel('t/s');
-ylabel('Amplitude/LSB');
-title('GYRO X Axis');
+if is_gyro == 1
+    data = load('./MPU6050_RawGyroData.txt');
+    x  = data(:, 1);
+    y1 = data(:, 2);
+    y2 = data(:, 3);
+    y3 = data(:, 4);
 
-subplot(3, 1, 2);
-plot(x,y2);
+    subplot(3, 1, 1);
+    plot(x, y1);
+    xlabel('t/s');
+    ylabel('Amplitude/LSB');
+    title('GYRO X Axis');
 
-xlabel('t/s');
-ylabel('Amplitude/LSB');
-title('GYRO Y Axis');
+    subplot(3, 1, 2);
+    plot(x, y2);
+    xlabel('t/s');
+    ylabel('Amplitude/LSB');
+    title('GYRO Y Axis');
 
-subplot(3, 1, 3);
-plot(x,y3);
+    subplot(3, 1, 3);
+    plot(x, y3);
+    xlabel('t/s');
+    ylabel('Amplitude/LSB');
+    title('GYRO Z Axis');
+else
+    data = load('./MPU6050_RawAccData.txt');
+    x  = data(:, 1);
+    y1 = data(:, 2);
+    y2 = data(:, 3);
+    y3 = data(:, 4);
 
-xlabel('t/s');
-ylabel('Amplitude/LSB');
-title('GYRO Z Axis');
+    subplot(3, 1, 1);
+    plot(x, y1);
+    xlabel('t/s');
+    ylabel('g/(m/s^2)');
+    title('ACC X Axis');
 
+    subplot(3, 1, 2);
+    plot(x, y2);
+    xlabel('t/s');
+    ylabel('g/(m/s^2)');
+    title('ACC Y Axis');
+
+    subplot(3, 1, 3);
+    plot(x, y3);
+    xlabel('t/s');
+    ylabel('g/(m/s^2)');
+    title('ACC Z Axis');
+end
